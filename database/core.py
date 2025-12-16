6dd1e974dc1d0b8436af4c2a999efdc9ae11316c
Wizer27 <svd2yar@gmail.com>
2025-12-16 22:33:53 +0300

    login / register

--- database/core.py
+++ database/core.py
@@ -14,4 +14,37 @@ def get_all_data():
             return res.fetchall()
         except Exception as e:
             return Exception(f"Error : {e}")
-print(get_all_data())
+def is_user_exists(username:str) -> bool:
+    with sync_engine.connect() as conn:
+        try:
+            stmt = select(table.c.username).where(table.c.username == username)
+            res = conn.execute(stmt)
+            data = res.fetchone()
+            return data[0] == username if data is not None else False
+        except Exception as e:
+            return Exception(f"Error : {e}")       
+def register(username:str,hash_psw:str) -> bool:
+    if is_user_exists(username):
+        return False
+    with sync_engine.connect() as conn:
+        try:
+            stmt = table.insert().values(
+                username = username,
+                hash_psw = hash_psw
+            )
+            conn.execute(stmt)
+            conn.commit()
+            return True
+        except Exception as e:
+            return Exception(f"Error : {e}")
+def login(username:str,hash_psw:str) -> bool:
+    if not is_user_exists(username):
+        return False
+    with sync_engine.connect() as conn:
+        try:
+            stmt = select(table.c.hash_psw).where(table.c.username == username)
+            res = conn.execute(stmt)
+            data = res.fetchone()
+            return data[0] == hash_psw if data is not None else False
+        except Exception as e:
+            return Exception(f"Error : {e}")
