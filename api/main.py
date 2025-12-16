ff8462dbd058925d7d1b7ab679d31878c4ba8ba7
Wizer27 <svd2yar@gmail.com>
2025-12-16 22:40:02 +0300

    api init

---
+++ api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI,Header,Depends,HTTPException,Request,status
+from pydantic import Field,BaseModel
+import uuid
+import hmac
+import hashlib
+from typing import List,Optional
+import uvicorn
+import json
+import os
+from dotenv import load_dotenv
+import time
+
+
+########## SECURITY ##########
+def verify_signature(data:dict,signature:str,timestamp:str) -> bool:
+    if int(time.time()) - int(timestamp) > 300:
+        return False
+    KEY = os.getenv("SIGNATURE")
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature",None)
+    data_str = json.dumps(data_to_verify,sort_keys = True,separators = (',',':'))
+    expected = hmac.new(KEY.encode(),data_str.encode(),hashlib.sha256).hexdigest()
+    return hmac.compare_digest(signature,expected)
+async def safe_get(req:Request):
+    api = req.headers.get("X-API-KEY")
+    api_key = os.getenv("API")
+    if not api or not hmac.compare_digest(api,api_key):
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN,detail = "invalid signature")
+
+
+########## INIT ##########
+load_dotenv()
+app = FastAPI()
+
+async def main():
+    return "LUX-SYNC"
+
