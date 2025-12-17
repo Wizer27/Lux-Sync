9edf1024dc0204eaea0d4f98f30d2c6d5c6737d4
Wizer27 <svd2yar@gmail.com>
2025-12-17 12:31:51 +0300

    files

--- database/files_databse/files_core.py
+++ database/files_databse/files_core.py
@@ -1,7 +1,22 @@
 from sqlalchemy import text,select
-from files_models import table,metadata_obj
-from files_models import connect,sync_engine
+from files_models import files_table,metadata_obj
+from files_sqli import sync_engine
+import uuid
 
 
 def create_table():
     metadata_obj.create_all(sync_engine)
+def create_new_user_file(username:str,file_name:str,file_data):
+    with sync_engine.connect() as conn:
+        try:
+            stmt = files_table.insert().values(
+                id = str(uuid.uuid4()),
+                owner = username,
+                filename = file_name,
+                data = file_data
+            )
+            conn.execute(stmt)
+            conn.commit()
+        except Exception as e:
+            return Exception(f"Error : {e}")
+        
