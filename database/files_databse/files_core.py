c4d0ec508d6c57986a524bd9ebd72fa569828a8b
Wizer27 <svd2yar@gmail.com>
2025-12-17 23:30:56 +0300

    some fixes

--- database/files_databse/files_core.py
+++ database/files_databse/files_core.py
@@ -4,8 +4,22 @@ from files_sqli import sync_engine
 import uuid
 
 
+
+
+
+
 def create_table():
     metadata_obj.create_all(sync_engine)
+
+def is_file_exists(file_id:str) -> bool:
+    with sync_engine.connect() as conn:
+        try:
+            stmt = select(files_table.c.id).where(files_table.c.id == file_id)
+            res = conn.execute(stmt)
+            data = res.fetchone()
+            return data[0] == file_id if data is not None else False
+        except Exception as e:
+            return Exception(f"Error : {e}")
 def create_new_user_file(username:str,file_name:str,file_data):
     with sync_engine.connect() as conn:
         try:
@@ -27,7 +41,9 @@ def get_user_files(username:str):
             return res.fetchall()
         except Exception as e:
             return Exception(f"Error : {e}")        
-def delete_user_file(file_id):
+def delete_user_file(file_id:str) -> bool:
+    if not is_file_exists(file_id):
+        return False
     with sync_engine.connect() as conn:
         try:
             stmt = files_table.update().where(files_table.c.id == file_id).values(
@@ -37,5 +53,19 @@ def delete_user_file(file_id):
             )
             conn.execute(stmt)
             conn.commit()
+            return True
         except Exception as e:
-            return Exception(f"Error : {e}")        
+            return Exception(f"Error : {e}")     
+def update_user_file_data(file_id:str,new_data) -> bool:
+    if not is_file_exists(file_id):
+        return False
+    with sync_engine.connect() as conn:
+        try:
+            stmt = files_table.update().where(files_table.c.id == file_id).values(
+                data = new_data
+            )
+            conn.execute(stmt)
+            conn.commit()
+            return True
+        except Exception as e:
+            return Exception(f"Error : {e}")           
