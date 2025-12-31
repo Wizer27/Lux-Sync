f895b09f092a4dc51e0a14b01365a2edfc63b4d8
Wizer27 <svd2yar@gmail.com>
2025-12-31 13:38:37 +0300

    small fix

--- file_scripts/file_memory.py
+++ file_scripts/file_memory.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 
 
+
 def count_size(file_name:str) -> int:
     try:
         filepath = Path(file_name)
         if filepath.exists():
             if filepath.is_file():
-                return int(filepath.stat().st_size)
+                return round(int(filepath.stat().st_size) / (1024 * 1024))
             else:
                 raise TypeError("This is not a file")
         else:
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import time
 from database.core import register,login
 from database.files_databse.files_core import create_new_user_file,get_user_files,delete_user_file,update_user_file_data,is_user_has_this_file
-
+from file_scripts.file_memory import count_size
 
 ########## SECURITY ##########
 def verify_signature(data:dict,signature:str,timestamp:str) -> bool:
@@ -23,6 +23,8 @@ def verify_signature(data:dict,signature:str,timestamp:str) -> bool:
     data_str = json.dumps(data_to_verify,sort_keys = True,separators = (',',':'))
     expected = hmac.new(KEY.encode(),data_str.encode(),hashlib.sha256).hexdigest()
     return hmac.compare_digest(signature,expected)
+
+
 async def safe_get(req:Request):
     api = req.headers.get("X-API-KEY")
     api_key = os.getenv("API")
@@ -40,7 +42,9 @@ async def main():
 class Register(BaseModel):
     username:str
     hash_psw:str
+
 @app.post("/register")
+
 async def register_api(req:Register,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED)
@@ -50,7 +54,10 @@ async def register_api(req:Register,x_signature:str = Header(...),x_timestamp:st
             raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Wrong data")
     except Exception as e:
         raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = f"Error : {e}")
+    
+
 @app.post("/login")
+
 async def login_api(req:Register,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED)
@@ -60,9 +67,15 @@ async def login_api(req:Register,x_signature:str = Header(...),x_timestamp:str =
             raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Wrong data")
     except Exception as e:
         raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = f"Error : {e}")
+    
+
 class GetUserFiles(BaseModel):
-    username:str    
-@app.post("/get/user/files")   
+    username:str   
+
+
+@app.post("/get/user/files")  
+
+
 async def get_user_files(req:GetUserFiles,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid signature")
@@ -71,11 +84,18 @@ async def get_user_files(req:GetUserFiles,x_signature:str = Header(...),x_timest
         return files
     except Exception as e:
         raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = f"Error : {e}") 
+    
+
+
 class UploadFile(BaseModel):
     username:str
     file_name:str
     file_data:str
+
+
 @app.post("/upload")
+
+
 async def upload_file(req:UploadFile,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid signature")
@@ -84,11 +104,17 @@ async def upload_file(req:UploadFile,x_signature:str = Header(...),x_timestamp:s
             raise HTTPException(status_code = status.HTTP_409_CONFLICT,detail = "File already exists")
         create_new_user_file(req.username,req.file_name,req.file_data)
     except Exception as e:
-        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = f"Error : {e}")       
+        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST,detail = f"Error : {e}")      
+    
+
 class DeleteFile(BaseModel):
     username:str
     file_name:str
+
+
 @app.post("/delete")
+
+
 async def delete(req:DeleteFile,x_signature:str = Header(...),x_timestamp:str = Header(...)):
     if not verify_signature(req.model_dump(),x_signature,x_timestamp):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid signature")
